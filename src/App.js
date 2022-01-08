46e30075cb152e74065d222beb99f825c4f6feaf
david12white <david12white@gmail.com>
2022-01-07 22:14:53 -0500

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+import './App.css';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="navbar">  
+                <a className="active" href="#home">Home</a> 
+                <a href="#projects">Projects</a> 
+                <a href="#contact">Contact</a> 
+                <a href="#resume">Resume</a>
+
+                <div className="Title">
+                    <ResumeTitle name="David White" number="919-624-3650" email="david12white@gmail.com" />
+                </div>
+            </div>
+            <div className="Introduction">
+                <img src ="Profile Picture.jpg" alt="Me" />
+            </div>
+
+            <div className="Statistics">
+                <h1> 4.0 </h1>
+                
+            </div>
+        </div>
+    );
+}
+class ResumeTitle extends React.Component {
+    render() {
+        return (
+            <header>
+                <h1>{this.props.name} | {this.props.number} | {this.props.email} </h1>
+            </header>
+        );
+    }
 }
 
 export default App;
